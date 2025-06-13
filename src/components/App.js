b0d6a21100dd0f935db06e32951a51d3397eb97a
Roshan Kumar <roshankumarrk9570@gmail.com>
2025-06-13 11:09:27 +0530

    add state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import './../styles/App.css';
 import ChildComponent1 from "./ChildComponent1";
 import ChildComponent2 from "./ChildComponent2";
+import { useState } from "react";
 
 const App = () => {
   const [selectedOption, setSelectedOption] = useState('');
